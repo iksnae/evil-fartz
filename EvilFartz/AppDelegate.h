f7b60d4017d8138abf2984a49871b8b48c70f1d8
Khalid <mustkre8@gmail.com>
2016-01-27 21:23:39 -0800

    Initial Commit

---
+++ EvilFartz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EvilFartz
+//
+//  Created by Khalid on 1/27/16.
+//  Copyright © 2016 iksnae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
